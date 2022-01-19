42cdf61d72ea331558fd04894c48d9834dcd0033
Nick Stoian <nts.stoian@gmail.com>
2022-01-19 09:57:51 +0000

    Initial Commit

---
+++ Tamagotchi/TamagotchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamagotchiApp.swift
+//  Tamagotchi
+//
+//  Created by Nick Stoian on 19/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct TamagotchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
